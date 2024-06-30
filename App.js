f5fb739bc60d3c749a27a9e0d6ac4f9e92d43603
janardhankandregula <janardhank134@gmail.com>
2024-06-30 01:28:13 -0400

    success

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const Header = () => {
+const AppLayout = () => {
   return (
     <div>
-      <h1>rendered</h1>
+      <HeaderComponent />
+      <BodyComponent />
     </div>
   );
 };
 
+const HeaderComponent = () => {
+  return (
+    <div className="container">
+      <div>
+        <img
+          className="logo"
+          src="https://logowik.com/content/uploads/images/free-food-delivery6258.logowik.com.webp"
+          alt="logoe"
+        />
+      </div>
+      <div className="nav-Bar">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const BodyComponent = () => {
+  return <div className="bodyContainer">Body</div>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<AppLayout />);
